2a80065d6316fe2ff1f9556365706a5ee7e52a56
e1b19041 <e1b19041@oit.ac.jp>
2021-10-11 17:17:59 +0900

    じゃんけん機能追加

---
+++ src/main/java/oit/is/z0160/kaizi/janken/model/Janken.java
@@ -0,0 +1,23 @@
+package oit.is.z0160.kaizi.janken.model;
+
+public class Janken{
+  public String hand;
+  public String cpuHand;
+  public String syouhai;
+
+  public Janken(String hand){
+    this.hand=hand;
+    this.cpuHand="Gu";
+
+    if(this.hand.equals("Gu")){
+      this.syouhai="Draw";
+    }
+    if(this.hand.equals("Choki")){
+      this.syouhai="You lose";
+    }
+    if(this.hand.equals("Pa")){
+      this.syouhai="You Win!";
+    }
+  }
+
+}
