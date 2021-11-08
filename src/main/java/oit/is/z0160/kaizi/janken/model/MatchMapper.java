95b9c65cf73fbcbe072e45622225b7a89855a4af
e1b19041 <e1b19041@oit.ac.jp>
2021-11-08 18:00:00 +0900

    じゃんけんの非同期処理を途中まで実装

--- src/main/java/oit/is/z0160/kaizi/janken/model/MatchMapper.java
+++ src/main/java/oit/is/z0160/kaizi/janken/model/MatchMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MatchMapper {
@@ -14,7 +15,16 @@ public interface MatchMapper {
   ArrayList<Match>
   selectAllMatch();
 
+  @Select("SELECT id,user1,user2,user1Hand,user2Hand from matches where isActive is true;")
+  Match selectByIsActive();
+
+  @Select("SELECT id,user1,user2,user1Hand,user2Hand from matches where id = #{id};")
+  Match selectById(int id);
+
   @Insert("INSERT INTO matches (user1,user2,user1Hand,user2Hand,isActive) VALUES (#{user1},#{user2},#{user1Hand},#{user2Hand},#{isActive});")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatch(Match match);
+
+  @Update("UPDATE MATCHES SET ISACTIVE=true, WHERE ID = #{id}")
+  void updateById(int id);
 }
